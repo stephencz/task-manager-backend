92b9d721252931712265c7f9bfb337d0cb853939
Stephen Czekalski <admin@stephencz.com>
2021-09-09 12:22:00 -0400

    Added endpoints for deleting tasks and soemthing else

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
 
-const db = require('./connect.js');
 const express = require('express');
+const cors = require('cors');
+const db = require('./connect.js');
 
 const app = express();
+
 const port = process.env.HOST_PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors())
+app.options('*', cors())
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.get('/tasks', (req, res) => {
+/**
+ * Gets all the tasks in the database.
+ */
+app.get('/tasks/get/all', (req, res) => {
   let con = db.make_connection()
   results = con.query("SELECT * FROM tasks;", function(err, result, something) {
     if( err ) {
@@ -20,14 +28,80 @@ app.get('/tasks', (req, res) => {
 
     res.send(result);
   });
-
 });
 
+/**
+ * Gets the task with the matching id.
+ */
+app.get('/tasks/get/id/:id', (req, res) => {
+  let con = db.make_connection()
+  const query = 'SELECT * FROM tasks WHERE task_id=' + req.params.id + ';';
 
-app.get('/createEmptyTask', (req, res) => {
-  res.send("Poopy");
+  results = con.query(query, function(err, result, something) {
+    if( err ) {
+      res.send(err)
+      throw err;
+    }
+
+    res.send(result);
+  });
+});
+
+/**
+ * Gets the Task with the highest primary key i.e. the latest task added to the
+ * database.
+ */
+app.get('/tasks/get/latest', (req, res) => {
+  let con = db.make_connection()
+  const query = 'SELECT * FROM tasks ORDER BY task_id DESC LIMIT 0, 1;';
+
+  results = con.query(query, function(err, result, something) {
+    if( err ) {
+      res.send(err)
+      throw err;
+    }
+
+    res.send(result);
+  });
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+/**
+ * Creates a new empty task.
+ */
+app.post('/tasks/new/empty', (req, res) => {
+  let con = db.make_connection()
+  const query = 'INSERT INTO tasks (task_description) VALUES ("This is an empty task.");';
+
+  results = con.query(query, function(err, result, something) {
+    if( err ) {
+      res.send(err)
+      throw err;
+    }
+
+    res.send(result);
+  });
+});
+
+app.delete('/tasks/delete/:selected', (req, res) => {
+
+  const selected  = req.query.selected;
+  const removed = []
+
+  let con = db.make_connection()
+
+  selected.forEach((elem) => {
+    const query = 'DELETE FROM tasks WHERE task_id=' + elem + ';';
+    results = con.query(query, function(err, result, something) {
+      if( err ) {
+        res.send(err)
+        throw err;
+      }  
+
+      removed.push(elem)
+    });
+  })
+
+  console.log(removed);
+
+  res.json(removed);
+});
