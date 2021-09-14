b99b12a9363d9ad4aaa28959d19fa1e4af91889b
Stephen Czekalski <admin@stephencz.com>
2021-09-14 12:39:11 -0400

    Added new tag, delete selected tags, and get latest tag endpoints to tag api

--- routes/tags.js
+++ routes/tags.js
@@ -12,9 +12,68 @@ router.get('/get/all', (req, res) => {
       throw err;
     }
 
+    console.log('Retrieved all tags');
     res.send(result);
   });
 
+  con.end();
+});
+
+router.get('/get/latest', (req, res) => {
+  let con = db.make_connection()
+  const query = 'SELECT * FROM tags ORDER BY tag_id DESC LIMIT 0, 1;';
+
+  con.query(query, function(err, result, something) {
+    if( err ) {
+      res.send(err)
+      throw err;
+    }
+
+    console.log('Fetched latest tag with tag_id ' + result[0].tag_id);
+    res.send(result);
+  });
+
+  con.end();
+})
+
+router.post('/new', (req, res) => {
+
+  let con = db.make_connection();
+
+  const query = 'INSERT INTO tags (tag_text, tag_fg, tag_bg) VALUES ("New Tag", "#ffffff", "#000000");';
+  con.query(query, (err, result, something) => {
+    if( err ) {
+      res.send(err)
+      throw err;
+    }
+
+    console.log("Created a new empty tag.");
+    res.sendStatus(200);
+  })
+
+  con.end();
+
+})
+
+/** Deletes tags with the matching task_ids from the database. */
+router.delete('/delete/selected', (req, res) => {
+
+  const selected  = req.query.selected;
+  let con = db.make_connection()
+
+  selected.forEach((element) => {
+    const query = 'DELETE FROM tags WHERE tag_id=' + element + ';';
+    con.query(query, function(err, result, something) {
+      if( err ) {
+        res.send(err)
+        throw err;
+      }  
+
+      console.log("Deleted tag with tag_id " + element + " from tags.");
+    });
+  })
+
+  res.sendStatus(200)
   con.end();
 });
 
