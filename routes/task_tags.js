0e85d43d10eb49d82ed13fde4e8b2b2ebc64b8c6
Stephen Czekalski <admin@stephencz.com>
2021-09-17 14:00:29 -0400

    Added endpoint api/v1/tasktags/get/all which returns a list of all task_tag relationships

--- routes/task_tags.js
+++ routes/task_tags.js
@@ -3,4 +3,20 @@ const express = require('express');
 const db = require('../connect.js');
 const router = express.Router();
 
+router.get('/get/all', (req, res) => {
+  let con = db.make_connection();
+
+  con.query('SELECT * FROM task_tags;', (err, result, something) => {
+    if( err ) {
+      res.send(err)
+      throw err;
+    }
+
+    console.log('Retrieved all task_tags.');
+    res.send(result);
+  });
+
+  con.end();
+});
+
 module.exports = router;
